b94aa9e1323ae3411491d34cc145033848b6aa16
Mark Watson <markwwork20@gmail.com>
2023-02-18 11:06:45 -0500

    Pizza CardCarousel

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import normalBake from "../images/normal bake.png";
+
+const Card = () => {
+  return (
+    <div className="mx-5 mb-10 w-96 shrink-0">
+      <img src={normalBake} className="w-full" />
+    </div>
+  );
+};
+
+export default Card;
