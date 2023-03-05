9e1c788d74ff6d1ba58ec65ff9003beb7885a3d8
Mark Watson <markwwork20@gmail.com>
2023-03-04 22:42:42 -0500

    Product Dev - Base v2

--- src/productDef/pizzaBuilder.js
+++ src/productDef/pizzaBuilder.js
@@ -3,6 +3,7 @@ const customizeRules = {
   productType: "Customer Pizza",
   ruleFlow: {
     ruleFlowId: "rfABC123",
+    ruleName: "Base Selections",
     flowPath: {
       id: "flowPath1",
       flow: [
@@ -15,7 +16,6 @@ const customizeRules = {
               "Crispy Parm",
               "Epic Stuffed",
               "Epic Pepperoni Stuffed",
-              "NY Style",
             ],
             childLevels: [
               {
@@ -23,6 +23,139 @@ const customizeRules = {
                 choiceHierarchy: {
                   choiceType: "Pizza Size",
                   choices: ["Small", "Medium", "Large", "X-Large"],
+                  childLevels: [],
+                },
+              },
+              {
+                choiceGroupId: "gc3",
+                choiceHierarchy: {
+                  choiceType: "Crust Flavor",
+                  choices: ["None", "Garlic Parmesan Cheese"],
+                  childLevels: [],
+                },
+              },
+              {
+                choiceGroupId: "gc4",
+                choiceHierarchy: {
+                  choiceType: "Sauce",
+                  choices: [
+                    "None",
+                    "BBQ",
+                    "Ranch",
+                    "Original",
+                    "Garlic",
+                    "Buffalo",
+                    "Alfredo Sauce",
+                  ],
+                  childLevels: [
+                    {
+                      choiceGroupId: "gc5",
+                      choiceHierarchy: {
+                        choiceType: "Sauce Amount",
+                        choices: ["Normal", "Light", "Extra"],
+                        childLevels: [],
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                choiceGroupId: "gc6",
+                choiceHierarchy: {
+                  choiceType: "Cut",
+                  choices: ["Normal Cut", "Square Cut", "Clean Cut", "No Cut"],
+                  childLevels: [],
+                },
+              },
+              {
+                choiceGroupId: "gc7",
+                choiceHierarchy: {
+                  choiceType: "Bake",
+                  choices: ["Normal Bake", "Well Done"],
+                  childLevels: [],
+                },
+              },
+            ],
+          },
+        },
+        {
+          choiceGroupId: "gc8",
+          choiceHierarchy: {
+            choiceType: "Crust",
+            choices: ["NY Style"],
+            childLevels: [
+              {
+                choiceGroupId: "gc9",
+                choiceHierarchy: {
+                  choiceType: "Pizza Size",
+                  choices: ["X-Large"],
+                  childLevels: [],
+                },
+              },
+              {
+                choiceGroupId: "gc3",
+                choiceHierarchy: {
+                  choiceType: "Crust Flavor",
+                  choices: ["None", "Garlic Parmesan Cheese"],
+                  childLevels: [],
+                },
+              },
+              {
+                choiceGroupId: "gc4",
+                choiceHierarchy: {
+                  choiceType: "Sauce",
+                  choices: [
+                    "None",
+                    "BBQ",
+                    "Ranch",
+                    "Original",
+                    "Garlic",
+                    "Buffalo",
+                    "Alfredo Sauce",
+                  ],
+                  childLevels: [
+                    {
+                      choiceGroupId: "gc5",
+                      choiceHierarchy: {
+                        choiceType: "Sauce Amount",
+                        choices: ["Normal", "Light", "Extra"],
+                        childLevels: [],
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                choiceGroupId: "gc6",
+                choiceHierarchy: {
+                  choiceType: "Cut",
+                  choices: ["Normal Cut", "Square Cut", "Clean Cut", "No Cut"],
+                  childLevels: [],
+                },
+              },
+              {
+                choiceGroupId: "gc7",
+                choiceHierarchy: {
+                  choiceType: "Bake",
+                  choices: ["Normal Bake", "Well Done"],
+                  childLevels: [],
+                },
+              },
+            ],
+          },
+        },
+
+        {
+          choiceGroupId: "gc12",
+          choiceHierarchy: {
+            choiceType: "Crust",
+            choices: ["Thin"],
+            childLevels: [
+              {
+                choiceGroupId: "gc13",
+                choiceHierarchy: {
+                  choiceType: "Pizza Size",
+                  choices: ["Small"],
                   childLevels: [],
                 },
               },
