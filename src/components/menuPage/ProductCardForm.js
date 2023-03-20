89013eb53830562e1458717c09f44111bebff700
Mark Watson <markwwork20@gmail.com>
2023-03-19 20:13:45 -0400

    Prodct card chages

--- src/components/menuPage/ProductCardForm.js
+++ src/components/menuPage/ProductCardForm.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import SelectField from "./SelectField";
-import SelectField2 from "./SelectField2";
-import SelectField3 from "./SelectField3";
-import SelectField4 from "./SelectField4";
 
 const ProductCardForm = ({ fields }) => {
   return (
