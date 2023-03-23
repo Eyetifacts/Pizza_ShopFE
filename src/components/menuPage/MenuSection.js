26f479842510b15c555b6b1fa4b74fee30ac9d1e
Claire Roberts <clairefinnie@gmail.com>
2023-03-23 10:48:05 -0400

    add banner card to menu page, naming edit to menu state

--- src/components/menuPage/MenuSection.js
+++ src/components/menuPage/MenuSection.js
@@ -1,31 +1,35 @@
-import React from "react";
-import ProductCard from "./ProductCard";
-import FancierProductCard from "./FancierProductCard";
+import React from "react"
+import ProductCard from "./ProductCard"
+import FancierProductCard from "./FancierProductCard"
+import BannerCard from "./BannerCard"
 
 const MenuSection = ({ sectionObject }) => {
-  return (
-    <section className="bg-slate-100">
-      <div className="ml-20 mt-5 text-5xl font-black uppercase">
-        {sectionObject.title}
-      </div>
-      <div className="flex flex-row ml-20 flex-wrap">
-        {sectionObject.productCards.map((productCard) => {
-          if (productCard.cardType === "ProductCard") {
-            return (
-              <ProductCard key={productCard.id} cardDetails={productCard} />
-            );
-          } else {
-            return (
-              <FancierProductCard
-                key={productCard.id}
-                cardDetails={productCard}
-              />
-            );
-          }
-        })}
-      </div>
-    </section>
-  );
-};
+	if (sectionObject.type === "bannerCard") {
+		return <BannerCard cardDetails={sectionObject.bannerCard} />
+	}
+	return (
+		<section className="bg-slate-100">
+			<div className="ml-20 mt-5 text-5xl font-black uppercase">
+				{sectionObject.title}
+			</div>
+			<div className="flex flex-row ml-20 flex-wrap">
+				{sectionObject.productCards.map((productCard) => {
+					if (productCard.cardType === "ProductCard") {
+						return (
+							<ProductCard key={productCard.id} cardDetails={productCard} />
+						)
+					} else {
+						return (
+							<FancierProductCard
+								key={productCard.id}
+								cardDetails={productCard}
+							/>
+						)
+					}
+				})}
+			</div>
+		</section>
+	)
+}
 
-export default MenuSection;
+export default MenuSection
