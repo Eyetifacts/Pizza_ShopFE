a6387578a499f3a9106e9eb2a9722b105171e496
Clay Carney <102885927+TheClutchC@users.noreply.github.com>
2023-03-29 11:07:54 -0400

    initial fetch request test FE to BE

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "../components/homePage/Hero";
 import CardCarousel from "../components/homePage/CardCarousel";
 import MenuMap from "../components/footer/MenuMap";
@@ -6,6 +6,27 @@ import MobileAppBanner from "../components/homePage/MobileAppBanner";
 import PromoCards from "../components/homePage/PromoCards";
 
 const HomePage = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(`http://localhost:3000/menu/menuItems`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setLoading(false);
+          setData(result);
+        })
+        .catch((e) => {
+          console.error(`An error occurred: ${e}`)
+        });
+  }, [])
+
   return (
     <>
       <Hero />
@@ -13,6 +34,10 @@ const HomePage = () => {
       <MenuMap />
       <MobileAppBanner />
       <PromoCards />
+      <div>
+        {loading && <p>Loading...</p>}
+        {!loading && data}
+      </div>
     </>
   );
 };
